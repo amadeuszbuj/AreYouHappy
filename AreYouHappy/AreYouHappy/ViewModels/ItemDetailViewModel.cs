2cfba1c66d35f85cb7d58f75d95930070bfb8a2e
Amadeusz Bujok <bujok.a@eplan.pl>
2020-02-04 19:59:46 +0100

    NEW: Clear repo on template added

---
+++ AreYouHappy/AreYouHappy/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using AreYouHappy.Models;
+
+namespace AreYouHappy.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
